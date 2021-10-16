f41fefbfb7871adabd7f99d7cd279633be3cd908
fenny <fenny@DESKTOP-OC5036T>
2021-10-16 20:06:36 +0300

    Add project files.

---
+++ RssTest/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace RssTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void rss()
+        {
+            
+            XmlTextReader xlmchz = new XmlTextReader(" "+link);
+            while (xlmchz.Read())
+            {
+                if (xlmchz.Name == "title")
+                {
+                    listBox1.Items.Add(xlmchz.ReadString());
+                }
+            }
+        }
+        string link;
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (textBox1.Text != "")
+            {
+                link = textBox1.Text;
+                rss();
+            }
+        }
+    }
+}
